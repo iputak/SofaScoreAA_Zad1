c2751934ce5ca62c2e0cf54f2156a076719fa3e9
iputak <iputak@hotmail.com>
2021-03-07 13:44:29 +0100

    Initial commit

---
+++ app/src/main/java/hr/vsite/map/sofascoreaa_zad1/MainActivity.kt
@@ -0,0 +1,11 @@
+package hr.vsite.map.sofascoreaa_zad1
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
